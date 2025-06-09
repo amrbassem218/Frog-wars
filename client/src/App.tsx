9aaeef89dc3780aaae5548fc0d15756c66cdef00
amrbassem218 <amrbeducation@gmail.com>
2025-06-09 03:18:35 +0300

    added extensions to files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Game from './Game'
-import Menu from './pages/menu'
+import Game from './Game.tsx'
+import Menu from './pages/menu.tsx'
 import { io } from 'socket.io-client'
 function App() {
   const [count, setCount] = useState(0)
